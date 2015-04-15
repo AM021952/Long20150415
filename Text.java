b17c4689bde6669dd6081c29d95f893dbd569ab0
AM021698 <chu_tae@hotmail.com.tw>
2015-04-15 11:35:08 +0800

    AM021698

--- Text.java
+++ Text.java
@@ -2,5 +2,6 @@ class Text{
 	public static void (String args[]){
 		System.out.println("資工二A AM021952 龍盛琳");
 		System.out.println("資工二A AM021700 羅恩培");
+		System.out.println("資工二A AM021698 朱泰瑞");
 	} 
 }
